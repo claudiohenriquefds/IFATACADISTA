4aaad353cabe487b3f0719e149eab43fe2a2aec9
DOCTOR <hacksboy@hotmail.com>
2020-04-13 18:54:45 -0300

    Primeiro commit do sistema

---
+++ IFATACADISTA_Backend/knexfile.js
@@ -0,0 +1,46 @@
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'developer',
+      password : '123456',
+      database : 'IFATACADISTA'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ IFATACADISTA_Backend/src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/',function(req, res) {
+    res.send('hello world');
+});
