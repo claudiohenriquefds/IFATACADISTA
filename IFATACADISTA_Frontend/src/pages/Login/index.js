1d21d13a62804eb7c170246c2a5c0436f0379fad
DOCTOR <hacksboy@hotmail.com>
2020-04-15 10:59:10 -0300

    Criação do layout da pagina de login

--- IFATACADISTA_Frontend/src/pages/Login/index.js
+++ IFATACADISTA_Frontend/src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiUser, FiLock } from 'react-icons/fi';
 
 import './style.css';
 
@@ -6,9 +7,17 @@ export default function Login(){
     return(
         <div className="container">
             <form>
-                <h1>IFATACADISTA LOGO</h1>
-                <input type="name" placeholder="Insira o seu usuário"/>
-                <input type="password" placeholder="Insira o sua senha"/>
+                <h1>Bem Vindo</h1>
+                <div className="input-with-icons">
+                    <FiUser size={20} color="#696969"/><input type="name" placeholder="Insira o seu usuário"/>
+                </div>
+                <div className="input-with-icons">
+                    <FiLock size={20} color="#696969"/><input type="password" placeholder="Insira o sua senha"/>
+                </div>
+                <div>
+                    <a href="#">Esqueceu a senha?</a>
+                    <a href="#">Não está cadastrado?</a>
+                </div>
                 <button type="submit">Logar</button>
             </form>
         </div>
